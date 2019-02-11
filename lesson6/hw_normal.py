776a49bcf39587f27385f15d3c7dd207042ec6e0
haro <okharatov@gmail.com>
2019-02-11 23:50:07 +0300

    Вторая задача готова

--- lesson6/hw_normal.py
+++ lesson6/hw_normal.py
@@ -17,8 +17,6 @@ class Person:
         self.health = health
         self.damage = damage
         self.armor = armor
-        self.wins = 0
-        self.lose = 0
 
     def attack(self, per):
         per_is_killed = False
@@ -27,8 +25,6 @@ class Person:
         per.health -= dmg
 
         if per.health <= 0:
-            per.lose += 1
-            self.wins += 1
             per_is_killed = True
 
         return per_is_killed
@@ -41,7 +37,60 @@ class Person:
 class Player(Person):
     def __init__(self, name, health, damage, armor):
         Person.__init__(self, name, health, damage, armor)
+        self.wins = 0
+        self.lose = 0
+
+    def choose_target(self, enemy_list):
+        return int(input('Введите номер врага для атаки '))
 
 class Enemy(Person):
     def __init__(self, name, health, damage, armor):
         Person.__init__(self, name, health, damage, armor)
+
+class GameClass():
+
+    def __init__(self, you):
+        # self.__set_params()
+
+        level = int(input(f"{player.name}, введите сложность от 1 до 3 "))
+        game_is_over = False
+        self.__enemys = [ Enemy(f'Enemy{e}', 60, 30, 2) for e in range(level)]
+        self.__print_enemy_info()
+        self.__enemys.append(you)
+
+        while not game_is_over:
+            for e in self.__enemys:
+                if isinstance(e, Enemy):
+                    game_is_over = e.attack(list(filter(lambda x: isinstance(x, Player), self.__enemys))[0])
+                    print(you.health)
+                    if game_is_over:
+                        break
+                else:
+                    self.__print_enemy_info()
+                    enem = self.__enemys[e.choose_target(list(filter(lambda x: isinstance(x, Enemy), self.__enemys)))]
+                    if e.attack(enem):
+                        self.__enemys.remove(enem)
+                        if len(self.__enemys) == 1:
+                            game_is_over = True
+                            break
+
+    def __print_enemy_info(self):
+        print(f'\nПротив вас играют противники:')
+        [print(f'{i}.   {e.name} , {e.health} жизней, {e.damage} урон, {e.armor} брони') 
+            for i, e in enumerate(self.__enemys) if isinstance(e, Enemy)]
+
+
+player = Player(input("Введите Ваше имя "), 100, 30, 5)
+
+while True:
+    GameClass(player)
+    answer = ""
+    if player.health <= 0:
+        answer = input("Вас убили, еще сыграем еще раз? (y/n)")
+    else:
+        answer = input("Вы победили, сыграем еще раз? (y/n)")
+
+    if answer != "y":
+        break
+    
+    player.health = 100
