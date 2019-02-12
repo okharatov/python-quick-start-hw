1503daf61dab5a43080e132b0a1122fcb7298863
haro <okharatov@gmail.com>
2019-02-12 10:03:51 +0300

    Третье задание готово

--- lesson6/hw_hard.py
+++ lesson6/hw_hard.py
@@ -11,3 +11,42 @@
 # Задача - 2
 # Доработайте нашу фабрику, создайте по одному классу на каждый тип, теперь надо в классе фабрика
 # исходя из типа игрушки отдавать конкретный объект класса, который наследуется от базового - Игрушка
+
+class Toy:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+class AnimalToy(Toy):
+    def __init__(self, name, color):
+        Toy.__init__(self, name, color)
+        self.toy_type = 'animal'
+
+class MovieToy(Toy):
+    def __init__(self, name, color):
+        Toy.__init__(self, name, color)
+        self.toy_type = 'movie'
+
+class Factory:
+    def __buy_material(self):
+        print('материал закуплен') 
+        
+    def __build(self):
+        print('игрушка сшита') 
+        
+    def __paint(self):
+        print('игрушка окрашена') 
+        
+    def make_toy(self, name, color, toy_type):
+        self.__buy_material() 
+        self.__build()
+        self.__paint()
+        
+        if toy_type == 'movie':
+            return MovieToy(name, color) 
+        else:
+            return AnimalToy(name, color)
+
+f = Factory()
+print(isinstance(f.make_toy('мишка', 'белый', 'animal'), AnimalToy))
+print(isinstance(f.make_toy('покемон', 'желтый', 'movie'), MovieToy))
